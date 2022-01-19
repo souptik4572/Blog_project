d30f41c0ee897954cee491a585a5cd2f817cb811
souptik4572 <souptiksarkar4572@gmail.com>
2022-01-19 07:55:35 +0530

    feat: add login functionality with email

--- users/forms.py
+++ users/forms.py
@@ -3,6 +3,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Profile
+from users.models import CustomUser
 
 
 class UserRegisterForm(UserCreationForm):
@@ -11,13 +12,13 @@ class UserRegisterForm(UserCreationForm):
     last_name = forms.CharField()
 
     class Meta:
-        model = User
+        model = CustomUser
         fields = ['username', 'email','first_name','last_name', 'password1', 'password2']
 
 class UserUpdateForm(forms.ModelForm):
      email = forms.EmailField()
      class Meta:
-        model = User
+        model = CustomUser
         fields = ['username', 'email','first_name','last_name',]
 
 
--- users/models.py
+++ users/models.py
@@ -2,20 +2,26 @@ from email.policy import default
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(_('email address'), max_length=250, unique=True)
+
 
 class Profile(models.Model):
-    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self):
+    def save(self, *args, **kwargs):
         super().save()
-        img=Image.open(self.image.path)
+        img = Image.open(self.image.path)
 
         if img.height > 300 or img.width > 300:
-            output_size=(300,300)
+            output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.image.path)
-
